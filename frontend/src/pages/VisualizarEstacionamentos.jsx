85f308f99fa2f4e5d1dc59946dbb9de17e9a359f
Sutanrrier <pabloryanbr0@gmail.com>
2022-07-04 15:13:14 -0300

    Visualizar Estacionamento's page now works

--- frontend/src/pages/VisualizarEstacionamentos.jsx
+++ frontend/src/pages/VisualizarEstacionamentos.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import "./Forms.css";
 
 function VisualizarEstacionamentos() {
+  //Pega o id do carro com base no endpoint atual da URL.
   let { id } = useParams();
-  return <h1>Visualizar Estacionamentos {id}</h1>;
+
+  const [dadosEstacionamento, setDadosEstacionamento] = useState([]);
+
+  const endpoint = `http://localhost:8080/estacionamentos/${id}`;
+
+  useEffect(() => {
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((dados) => {
+        setDadosEstacionamento(dados);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="form-title">Visualizando Estacionamento - ID: {id}</h1>
+      <div className="formularioPaginas">
+        <label htmlFor="cor">Nome do estacionamento: </label>
+        <input
+          name="nome"
+          type="text"
+          readOnly="readonly"
+          value={dadosEstacionamento.nome}
+        />
+
+        <label htmlFor="estacionamento_id">Cadastrado em: </label>
+        <input
+          name="dataCriacao"
+          type="date"
+          readOnly="readonly"
+          value={dadosEstacionamento.dataCriacao}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default VisualizarEstacionamentos;
