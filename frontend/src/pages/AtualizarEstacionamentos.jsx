956834f407681b4b23ecccbe21b388e92086637d
Sutanrrier <pabloryanbr0@gmail.com>
2022-07-04 15:07:37 -0300

    Atualizar estacionamento's page now works

--- frontend/src/pages/AtualizarEstacionamentos.jsx
+++ frontend/src/pages/AtualizarEstacionamentos.jsx
@@ -1,11 +1,68 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 import "./Forms.css";
 
 function AtualizarEstacionamentos() {
+  //Pega o id do carro com base no endpoint atual da URL.
   let { id } = useParams();
-  return <h1>Atualizar Estacionamentos {id}</h1>;
+
+  let navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-type", "application/json; charset=UTF-8");
+
+    const endpoint = `http://localhost:8080/estacionamentos/${id}`;
+    const body = {
+      id: data.id,
+      nome: data.nome,
+    };
+
+    const options = {
+      method: "PUT",
+      body: JSON.stringify(body),
+      mode: "cors",
+      cache: "default",
+      headers: myHeaders,
+    };
+
+    fetch(endpoint, options).then(() => {
+      alert("Estacionamento atualizado com sucesso!");
+      navigate("/");
+    });
+  };
+
+  return (
+    <div className="container">
+      <h1 className="form-title">Atualizar Estacionamento</h1>
+      <div className="formularioPaginas">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="number"
+            hidden
+            name="id"
+            value={id}
+            {...register("id")}
+            required
+          />
+
+          <label htmlFor="nome">Novo nome: </label>
+          <input
+            name="nome"
+            type="text"
+            placeholder="Digite o novo nome do estacionamento"
+            {...register("nome")}
+            required
+          />
+
+          <button type="submit">Atualizar Dados</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default AtualizarEstacionamentos;
